58e6fcb43f4b3faedf5886784f86df938947af94
mhaut <juanmariohaut@unex.es>
2018-04-26 12:35:59 +0200

    add practica

---
+++ practica8.py
@@ -0,0 +1,18 @@
+import re
+from pyspark import SparkContext, SparkConf
+conf = SparkConf().setAppName("contar palabras")
+sc = SparkContext(conf=conf)
+
+verbose = "ERROR"
+
+if verbose == "info":
+	logleveltype = "INFO"
+elif verbose == "warning":
+	logleveltype = "WARN"
+else:
+	logleveltype = "ERROR"
+sc.setLogLevel(logleveltype)
+
+
+for act_dict in ACCION:
+	print act_dict[0] + ":", act_dict[1]
